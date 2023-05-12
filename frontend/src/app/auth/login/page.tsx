bc79262f4bc3c2cacc8af7cc98fe5f0a645809e4
Azrod <tom.sorabella@gmail.com>
2023-05-12 10:50:43 +0200

    ADD joi for form input validation on login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,12 +1,23 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './login.module.css';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Image from 'next/image';
 import pitch from '../../../../public/media/logo-login.png'
 import Link from 'next/link';
 import { sign } from 'crypto';
+import * as Joi from 'joi';
 
+
+const schema = Joi.object ({
+	username: Joi.string()
+		.alphanum()
+		.min(3)
+		.max(30)
+		.required(),
+	password: Joi.string()
+		.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+})
 
 type FormValues = {
 	username: string;
@@ -14,20 +25,46 @@ type FormValues = {
   };
 
 interface LoginProps {
-
 }
+
+
 
 const Login: React.FC<LoginProps> = ({}) => {
 	const { register, handleSubmit } = useForm<FormValues>();
-	const onSubmit: SubmitHandler<FormValues> = data => (
-		console.log(data)
-	);
+	const [isDisplay, setDisplay] = useState(false);
+
+	function toggleDisplayOn() {setDisplay((isDisplay) => isDisplay = true);}
+	function toggleDisplayOff() {setDisplay((isDisplay) => isDisplay = false);}
+
+
+	const onSubmit: SubmitHandler<FormValues> = data => {
+		const value = schema.validate(data);
+		if (value.error){
+			document.getElementById("testt")?.classList.add("test");
+			setTimeout(() => {
+				toggleDisplayOn();
+			}, 600);
+			setTimeout(() => {
+				let errorTypeEl: HTMLElement | null = document.getElementById("error-type")
+				if (errorTypeEl)
+					errorTypeEl.innerText = value.error.details['0'].context?.key!;
+			}, 1000);
+		}else {
+			toggleDisplayOff()
+		}
+
+	};
 	return(
 	<div className={styles.maindiv}>
 		<Image src={pitch} alt="nintendo" width={95} height={66} priority={true}/>
+		{isDisplay &&
+		<div className={styles.errorMessage}>
+			<span>Incorrect format on </span><span id="error-type"></span>
+		</div>
+		}
 		<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-			<div className={styles.inpuetEl}>
-		 		<label className={styles.labelText}>Nickname :</label>
+			<div id="testt" className={styles.inpuetEl}>
+		 		<label className={styles.labelText}>Username :</label>
     			<input className={styles.inputText} {...register("username")} />
 			</div>
 			<div className={styles.inpuetEl}>
